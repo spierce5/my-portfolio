fdd395ee2171d774e1170022352fda08eab065ed
Sam <sam.pierce76@gmail.com>
2023-04-02 17:30:04 -0400

    Improvements to photo editor layout

--- components/ImageCropper.js
+++ components/ImageCropper.js
@@ -1,6 +1,6 @@
 import Cropper from "react-easy-crop";
 import { useState, useCallback } from "react";
-import { Container, Stack, Slider } from "@mui/material";
+import { Container, Stack, Slider, Typography } from "@mui/material";
 
 export default function ImageCropper(props) {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
@@ -22,24 +22,41 @@ export default function ImageCropper(props) {
   );
 
   return (
-    <Container sx={{ height: "100vh", position: "relative" }}>
-      <Cropper
-        className="absolute"
-        image={props.src}
-        crop={crop}
-        zoom={zoom}
-        aspect={4 / 3}
-        onCropChange={setCrop}
-        onCropComplete={onCropComplete}
-        onZoomChange={setZoom}
-      />
-      <Slider
-        className="w-4/5 md:w-2/5 absolute mb-2"
-        defaultValue={zoom}
-        aria-label="Default"
-        valueLabelDisplay="auto"
-        onChange={handleSlide}
-      />
-    </Container>
+    <>
+      <Container className="h-5/6" sx={{ position: "relative" }}>
+        <Cropper
+          className=""
+          image={props.src}
+          crop={crop}
+          zoom={zoom}
+          aspect={9 / 16}
+          cropSize={{ width: 450, height: 711 }}
+          onCropChange={setCrop}
+          onCropComplete={onCropComplete}
+          onZoomChange={setZoom}
+        />
+        <div className="h-full flex flex-row items-center">
+          <Typography
+            className="-rotate-90"
+            variant="subtitle1"
+            sx={{ color: "white" }}
+          >
+            Zoom
+          </Typography>
+          <Slider
+            className="h-1/3 mt-2 mb-2"
+            orientation="vertical"
+            color="tertiary"
+            defaultValue={zoom}
+            aria-label="Default"
+            valueLabelDisplay="auto"
+            onChange={handleSlide}
+            min={1}
+            max={3}
+            step={0.1}
+          />
+        </div>
+      </Container>
+    </>
   );
 }
--- components/PhotoEditor.js
+++ components/PhotoEditor.js
@@ -1,20 +1,39 @@
 import { useState, useCallback, useEffect } from "react";
 import { Stack, Button, Typography, Container } from "@mui/material";
-import { uploadFile, getFile } from "../firebase/firebase.js";
+import { uploadFile, getFile, getContent } from "../firebase/firebase.js";
+import ImageCropper from "../components/ImageCropper";
 
 export default function PhotoEditor(props) {
   const [image, setImage] = useState({
-    url: props.url,
+    fileName: null,
+    url: "",
     newFile: null,
   });
   const [isNewFile, setIsNewFile] = useState(false);
 
   useEffect(() => {
-    /*
-    Retrieve URL from realtime database. URL will be stored on upload to avoid 
-    using regex to search file names without extensions each time
-    */
-  }, []);
+    if (!image.fileName) {
+      getContent(setFileName);
+    }
+    if (image.fileName) {
+      getFile(image.fileName).then((imageURL) => {
+        setImage({
+          url: imageURL,
+          newFile: null,
+        });
+      });
+    }
+  }, [image, setImage]);
+
+  const setFileName = useCallback(
+    (content) => {
+      let fileName = content.images[props.reference].file_name;
+      const updatedImage = { ...image };
+      updatedImage["fileName"] = fileName;
+      setImage(updatedImage);
+    },
+    [props.reference, image, setImage]
+  );
 
   const handleFileUpload = useCallback(() => {
     const path = "/curriculum_vitae.pdf";
@@ -41,10 +60,12 @@ export default function PhotoEditor(props) {
   );
 
   return (
-    <Container className="h-screen">
-      <Stack spacing={1} className="h-full">
-        <Typography variant="h3">{props.title}</Typography>
-        <div className="flex-col space-y-28 md:space-y-12 h-full">
+    <Container maxWidth={false} className="h-screen">
+      <div className="flex flex-col md:flex-row h-full">
+        <Stack spacing={1} className="h-full md:w-1/4">
+          <Typography variant="h3">Photo Editor</Typography>
+          <Typography variant="h5">{props.title}</Typography>
+
           <Stack direction="row" spacing={1} className="justify-end">
             <Button component="label" variant="outlined">
               Upload
@@ -63,9 +84,11 @@ export default function PhotoEditor(props) {
               Publish
             </Button>
           </Stack>
-          <img src={image.url} alt={props.reference} />
+        </Stack>
+        <div className="flex flex-col justify-center space-y-28 md:space-y-12 w-full h-full">
+          <ImageCropper src={image.url} />
         </div>
-      </Stack>
+      </div>
     </Container>
   );
 }
