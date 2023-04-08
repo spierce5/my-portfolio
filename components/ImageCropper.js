fd97ea5fa803eff91bd14d3dfb99b585f4c89ef5
Sam <sam.pierce76@gmail.com>
2023-04-08 00:38:06 -0400

    controls outside of ImageCroper in nice list with upload, discard, and save buttons.

--- components/ImageCropper.js
+++ components/ImageCropper.js
@@ -4,22 +4,20 @@ import { Container, Stack, Slider, Typography } from "@mui/material";
 
 export default function ImageCropper(props) {
   const [crop, setCrop] = useState({ x: 0, y: 0 });
-  const [zoom, setZoom] = useState(1);
 
   const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
     console.log(croppedArea, croppedAreaPixels);
   }, []);
 
-  const handleSlide = useCallback(
-    (e) => {
-      const target = e.target;
-      const value = target.value;
-      if (value >= 1) {
-        setZoom(value);
-      }
-    },
-    [setZoom]
-  );
+  const handleZoom = (val) => {
+    const e = {
+      target: {
+        name: "zoom",
+        value: val,
+      },
+    };
+    props.handleZoom(e);
+  };
 
   return (
     <>
@@ -28,34 +26,14 @@ export default function ImageCropper(props) {
           className=""
           image={props.src}
           crop={crop}
-          zoom={zoom}
+          zoom={props.controls.zoom}
+          rotation={props.controls.rotation}
           aspect={9 / 16}
           cropSize={{ width: 450, height: 711 }}
           onCropChange={setCrop}
           onCropComplete={onCropComplete}
-          onZoomChange={setZoom}
+          onZoomChange={handleZoom}
         />
-        <div className="h-full flex flex-row items-center">
-          <Typography
-            className="-rotate-90"
-            variant="subtitle1"
-            sx={{ color: "white" }}
-          >
-            Zoom
-          </Typography>
-          <Slider
-            className="h-1/3 mt-2 mb-2"
-            orientation="vertical"
-            color="tertiary"
-            defaultValue={zoom}
-            aria-label="Default"
-            valueLabelDisplay="auto"
-            onChange={handleSlide}
-            min={1}
-            max={3}
-            step={0.1}
-          />
-        </div>
       </Container>
     </>
   );
--- components/PhotoEditor.js
+++ components/PhotoEditor.js
@@ -5,15 +5,19 @@ import {
   Button,
   Typography,
   Container,
-  List,
-  ListItem,
-  ListItemButton,
+  MenuItem,
+  MenuList,
+  ListItemIcon,
   ListItemText,
-  ListSubheader,
+  Paper,
+  Slider,
   Divider,
 } from "@mui/material";
 import { uploadFile, getFile, getContent } from "../firebase/firebase.js";
 import ImageCropper from "../components/ImageCropper";
+import SaveIcon from "@mui/icons-material/Save";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import ClearIcon from "@mui/icons-material/Clear";
 
 export default function PhotoEditor(props) {
   const theme = useTheme();
@@ -23,6 +27,10 @@ export default function PhotoEditor(props) {
     newFile: null,
   });
   const [isNewFile, setIsNewFile] = useState(false);
+  const [controls, setControls] = useState({
+    zoom: 1,
+    rotation: 0,
+  });
 
   useEffect(() => {
     if (!image.fileName) {
@@ -72,6 +80,27 @@ export default function PhotoEditor(props) {
     [image, setImage]
   );
 
+  const handleSlide = useCallback(
+    (e) => {
+      const target = e.target;
+      const name = target.name;
+      const value = target.value;
+      const newControls = { ...controls };
+      newControls[name] = value;
+      setControls(newControls);
+    },
+    [controls, setControls]
+  );
+
+  const actions = [
+    {
+      icon: <FileUploadIcon color="tertiary" fontSize="small" />,
+      name: "Upload",
+    },
+    { icon: <ClearIcon color="tertiary" fontSize="small" />, name: "Discard" },
+    { icon: <SaveIcon color="tertiary" fontSize="small" />, name: "Save" },
+  ];
+
   return (
     <Container maxWidth={false} className="h-screen">
       <div className="flex flex-col md:flex-row h-full">
@@ -99,41 +128,65 @@ export default function PhotoEditor(props) {
               Publish
             </Button>
           </Stack>
-          <List
-            className="max-w-max border-2"
-            sx={{ color: "primary.main" }}
-            subheader={
-              <ListSubheader
-                className="text-center"
-                component="div"
-                id="nested-list-subheader"
+          <Paper sx={{ width: 320, maxWidth: "100%" }}>
+            <MenuList
+              sx={{ backgroundColor: "primary.main", color: "tertiary.main" }}
+            >
+              {actions.map((action) => (
+                <MenuItem key={action.name}>
+                  <ListItemIcon>{action.icon}</ListItemIcon>
+                  <ListItemText>{action.name}</ListItemText>
+                </MenuItem>
+              ))}
+              <Divider />
+              <MenuItem key="zoom-slider" className="flex flex-col items-start">
+                <Typography id="zoom-label" gutterBottom={true}>
+                  Zoom
+                </Typography>
+                <Slider
+                  name="zoom"
+                  className=""
+                  color="tertiary"
+                  defaultValue={0}
+                  value={controls.zoom}
+                  onChange={handleSlide}
+                  aria-labelledby="zoom-label"
+                  valueLabelDisplay="auto"
+                  valueLabelFormat={(val) => Math.round(val * 100) + "%"}
+                  min={1}
+                  max={3}
+                  step={0.1}
+                />
+              </MenuItem>
+              <MenuItem
+                key="rotation-slider"
+                className="flex flex-col items-start"
               >
-                Options
-              </ListSubheader>
-            }
-          >
-            <Divider light={false} />
-            <ListItemButton>
-              <ListItemText primaryTypographyProps={{ variant: "button" }}>
-                Upload New Image
-              </ListItemText>
-            </ListItemButton>
-            <Divider light={false} />
-            <ListItemButton>
-              <ListItemText primaryTypographyProps={{ variant: "button" }}>
-                Discard Changes
-              </ListItemText>
-            </ListItemButton>
-            <Divider light={false} />
-            <ListItemButton>
-              <ListItemText primaryTypographyProps={{ variant: "button" }}>
-                Publish Cropped Image
-              </ListItemText>
-            </ListItemButton>
-          </List>
+                <Typography id="rotation-label" gutterBottom={true}>
+                  Rotation
+                </Typography>
+                <Slider
+                  name="rotation"
+                  className=""
+                  color="tertiary"
+                  defaultValue={0}
+                  onChange={handleSlide}
+                  aria-labelledby="rotation-label"
+                  valueLabelDisplay="auto"
+                  min={-90}
+                  max={90}
+                  step={1}
+                />
+              </MenuItem>
+            </MenuList>
+          </Paper>
         </Stack>
-        <div className="flex flex-col justify-center space-y-28 md:space-y-12 w-full h-full">
-          <ImageCropper src={image.url} />
+        <div className="flex flex-row justify-center items-center space-y-28 md:space-y-12 w-full h-full">
+          <ImageCropper
+            src={image.url}
+            controls={controls}
+            handleZoom={handleSlide}
+          />
         </div>
       </div>
     </Container>
