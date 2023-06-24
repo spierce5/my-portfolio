1b71063e9b41745e29f2eb608d1b573831a98963
Sam <sam.pierce76@gmail.com>
2023-06-24 00:51:50 -0400

    created basic pdf using react-pdf and created josn containing resume data

---
+++ components/ResumePDF.js
@@ -0,0 +1,31 @@
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+
+export default function ResumePDF() {
+  const styles = StyleSheet.create({
+    page: {
+      display: "flex",
+      flexDirection: "column",
+      backgroundColor: "#E4E4E4",
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+    },
+  });
+
+  return (
+    <div className="m-12 flex flex-col justify-center items-center">
+      <Document>
+        <Page size="A4" style={styles.page}>
+          <View style={styles.section}>
+            <Text>Section #1</Text>
+          </View>
+          <View style={styles.section}>
+            <Text>Section #2</Text>
+          </View>
+        </Page>
+      </Document>
+    </div>
+  );
+}
---
+++ pages/test.js
@@ -0,0 +1,9 @@
+import ResumePDF from "../components/ResumePDF";
+
+export default function Test() {
+  return (
+    <div className="m-12 flex flex-col justify-center items-center">
+      <ResumePDF />
+    </div>
+  );
+}
